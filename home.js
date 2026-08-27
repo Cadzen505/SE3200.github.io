ac3f0e548eb1a7c3d8a4cf1b2a3d47785dfb8f66
Caden <Caden@cloudg.com>
2026-08-27 15:16:19 -0600

    btn

---
+++ home.js
@@ -0,0 +1,7 @@
+const halo_img = document.querySelector('#halo');
+const btn = document.querySelector('#nxt_btn');
+
+let halo_imgs = [
+    https://raw.githubusercontent.com/Cadzen505/SE3200.github.io/main/halo1.jpeg
+    https://raw.githubusercontent.com/Cadzen505/SE3200.github.io/main/best_halo.jpeg
+]
