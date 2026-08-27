668628bc0d6ff751465b787cc91e51fd2195222a
Caden <Caden@cloudg.com>
2026-08-27 15:52:20 -0600

    btn implemented and working

--- home.js
+++ home.js
@@ -1,7 +1,23 @@
-const halo_img = document.querySelector('#halo');
+const halo_img = document.querySelector('#halo_img');
 const btn = document.querySelector('#nxt_btn');
 
 let halo_imgs = [
-    https://raw.githubusercontent.com/Cadzen505/SE3200.github.io/main/halo1.jpeg
-    https://raw.githubusercontent.com/Cadzen505/SE3200.github.io/main/best_halo.jpeg
+    "https://raw.githubusercontent.com/Cadzen505/SE3200.github.io/main/halo1.webp",
+    "https://raw.githubusercontent.com/Cadzen505/SE3200.github.io/main/best_halo.jpeg",
+    "https://raw.githubusercontent.com/Cadzen505/SE3200.github.io/main/halo3_odst.webp",
+    "https://raw.githubusercontent.com/Cadzen505/SE3200.github.io/main/halo3.jpeg",
+    "https://raw.githubusercontent.com/Cadzen505/SE3200.github.io/main/halo_reach.jpeg"
 ]
+
+let img_index = 0;
+
+btn.addEventListener('click', function() {
+
+    img_index++;
+
+    if(img_index >= halo_imgs.length){
+        img_index = 0;
+    }
+
+    halo_img.src = halo_imgs[img_index];
+});
